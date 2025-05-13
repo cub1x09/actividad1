93256aab8fa95a050e2eb8dc035c25b8b054d7b9
vishy anand <l23140682@queretaro.tecnm.mx>
2025-05-13 05:06:17 -0600

    Update hola_mundo.asm

--- ejercicio-uno/hola_mundo.asm
+++ ejercicio-uno/hola_mundo.asm
@@ -1,19 +1,17 @@
 section .data
-    msg db "Hola Mundo!", 0xa  ; La cadena de texto y un salto de línea (0xa)
-    len equ $ - msg           ; Calcula la longitud de la cadena automáticamente
+    msg db "Hola Mundo!", 0xa
+    len equ $ - msg
 
 section .text
-    global _start             ; Punto de entrada para el enlazador
+    global _start
 
 _start:
-    ; Syscall para escribir en pantalla (sys_write)
-    mov eax, 4                ; Número de la llamada al sistema sys_write (32 bits)
-    mov ebx, 1                ; Descriptor de archivo 1 (stdout - salida estándar)
-    mov ecx, msg              ; Dirección del mensaje a escribir
-    mov edx, len              ; Longitud del mensaje
-    int 0x80                  ; Llamada al kernel para ejecutar la syscall
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msg
+    mov edx, len
+    int 0x80
 
-    ; Syscall para salir del programa (sys_exit)
-    mov eax, 1                ; Número de la llamada al sistema sys_exit (32 bits)
-    mov ebx, 0                ; Código de salida 0 (sin errores)
-    int 0x80                  ; Llamada al kernel para ejecutar la syscall
+    mov eax, 1
+    mov ebx, 0
+    int 0x80
